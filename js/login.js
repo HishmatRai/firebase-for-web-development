0a0c320ac7ffb04e438253f06674514db2b46e5b
hishmatrai <hishmatrai.2018@gmail.com>
2025-10-26 18:06:19 +0500

    update

--- js/login.js
+++ js/login.js
@@ -1,22 +1,24 @@
 let email = document.getElementById("email");
 let password = document.getElementById("password");
 let message = document.getElementById("message");
-const SignUp = () => {
+let btn = document.getElementById("btn");
+const LoginHandler = () => {
+  btn.value = "Loading ...";
   firebase
     .auth()
-    .createUserWithEmailAndPassword(email.value, password.value)
+    .signInWithEmailAndPassword(email.value, password.value)
     .then((res) => {
       console.log(res.user);
       message.innerHTML = "Success!";
       message.style.color = "green";
+      btn.value = "Log In";
       // move
-      window.location.assign("./pages/home.html");
+        // window.location.assign("./home.html");
     })
     .catch((error) => {
-      console.log(error.message);
+      console.log(error);
       message.innerHTML = error.message;
       message.style.color = "red";
+      btn.value = "Log In";
     });
 };
-
-
