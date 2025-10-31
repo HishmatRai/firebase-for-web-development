8b18ecf22643993f30cddc3f903fc6f41698c0b5
hishmatrai <hishmatrai.2018@gmail.com>
2025-10-31 18:16:33 +0500

    update

--- js/home.js
+++ js/home.js
@@ -1,13 +1,45 @@
 // realtime databse add data
-
+let input = document.getElementById("input");
 const addHandler = () => {
+  //
+  // firebase
+  //   .database()
+  //   .ref("todos/" + "todo-1")
+  //   .set({
+  //     todoValue: input.value,
+  //   });
+
   firebase
     .database()
-    .ref("users/" + "user6")
-    .set({
-      name: "user6",
-      email: "user6@gmail.com",
-      age: 36,
-      isStudent: false,
+    .ref("todos/")
+    .push({
+      todoValue: input.value,
+    })
+    .then(() => {
+      input.value = "";
     });
 };
+
+// get data from realtime database
+  // firebase
+  // .database()
+  // .ref("todos/" + "-OcuHnvpaViPWlA3AJ9l")
+  // .on("value", (todoRes) => {
+  //   console.log(todoRes.val());
+  // });
+  
+
+
+  firebase
+  .database()
+  .ref("todos/" )
+  .on("value", (todoRes) => {
+    //
+    todoRes.forEach((todo) => {
+      console.log(todo.val());
+    });
+  });
+
+
+
+
